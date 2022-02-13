68c1f662666f9b1b8dd347573fbb197a49969798
Dhiraj Kumar Gautam <69205225+dkg44@users.noreply.github.com>
2022-02-13 12:38:34 +0530

    11feb--2nd

---
+++ Stack/11feb_validParanthesis.cpp
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        int n = s.length();
+        stack<int> st;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if (st.empty())
+                    return false;
+
+                char c = st.top();
+                st.pop();
+                if ((s[i] == ')' && c == '(') || (s[i] == ']' && c == '[') || (s[i] == '}' && c == '{'))
+                {
+                    // return true;
+                    continue;
+                }
+                else
+                    return false;
+            }
+        }
+        if (st.empty())
+            return true;
+        else
+            return false;
+    }
+};
